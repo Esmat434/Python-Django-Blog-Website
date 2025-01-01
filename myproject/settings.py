b7efb6138133a1eec6061f9964c6fa143d493179
Esmat434 <craypto4@gmail.com>
2025-01-01 08:39:26 -0800

    Update settings.py
    Add the check user activity middleware and Add the page view middleware

--- myproject/settings.py
+++ myproject/settings.py
@@ -49,6 +49,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "myapp.middleware.CheckUserActivityDateMiddleware",
+    "myapp.middleware.PageViewMiddleware",
 ]
 
 ROOT_URLCONF = "myproject.urls"
