a7dd7ba279ed6678e558b44a80bdc14071644ce9
Esmat434 <craypto4@gmail.com>
2025-01-01 08:21:15 -0800

    Update admin.py
    Create model Admin class for all model

--- myapp/admin.py
+++ myapp/admin.py
@@ -2,10 +2,30 @@ from django.contrib import admin
 from .models import Post,Comment,Contact
 # Register your models here.
 
-admin.site.register(Post)
-admin.site.register(Comment)
-admin.site.register(Contact)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id','user','category','postname','time']
+    list_filter = ['user','category']
+    search_fields = ['user','postname','category']
+    ordering = ['user','category']
 
+@admin.register(View)
+class ViewAdmin(admin.ModelAdmin):
+    list_display = ['id','post','view','client_ip','path']
+    list_filter = ['post']
+    search_fields = ['post']
+    
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['id','user','post','time']
+    list_filter = ['user','post']
+    search_fields = ['user','post']
+    ordering = ['time']
+
+@admin.register(Contact)
+class ContentAdmin(admin.ModelAdmin):
+    list_display = ['id']
 
 
 admin.site.site_header = 'BLOGSPOT | ADMIN PANEL'
